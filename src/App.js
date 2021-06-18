77978b8f8b3a5621595b16b37cf52228de774631
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-17 21:07:22 -0400

    Add web3 network to app, but need metamask install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react'
+import Web3 from 'web3'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.loadBlockchainData()
+  }
+
+ async loadBlockchainData() {
+   const web3 = new Web3('http://localhost:8545')
+   const network = await web3.eth.net.getNetworkType()
+   const accounts = await web3.eth.getAccounts()
+   console.log('account', accounts, 'network', network)
+ }
+
+ constructor(props) {
+   super(props)
+   this.state = {
+     account: ''
+   }
+ }
+  render() {
+    return (
+      <div className="container">
+        <h1> Hello, World! </h1>
+        <p>  Your account: [account from state]</p>
+      </div>
+    );
+  }
 }
 
 export default App;
