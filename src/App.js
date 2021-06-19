10c9658f4c39774d7084238f8b5b37c34a5f0d82
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-18 20:55:28 -0400

    Stuck at adding tasks for account

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
 import './App.css';
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
 
 class App extends Component {
   componentDidMount() {
@@ -17,20 +18,28 @@ class App extends Component {
    // allows metamask to talk to react app... updated where u need permission to view account info
    // await window.ethereum.enable();
    this.setState({ account: accounts })
-   console.log('account', accounts, 'network', network)
+   const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+   this.setState({ todoList })
+   const taskCount = await todoList.methods.taskCount().call()
+   this.setState({ taskCount })
+   console.log('network', network)
+   console.log('todolist', todoList)
+
  }
 
  constructor(props) {
    super(props)
    this.state = {
-     account: ''
+     account: '',
+     taskCount: 0
    }
  }
   render() {
     return (
       <div className="container">
         <h1> Hello, World! </h1>
-        <p>  Your account: {this.state.account}</p>
+        <p>Task Count: {this.state.taskCount}  </p>
+        <p>Your account: {this.state.account}</p>
       </div>
     );
   }
